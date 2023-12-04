7d042bcdb78cd42be9fea250ff8aa77f1a366c3f
locpham92 <phamquyloc92@gmail.com>
2023-12-04 18:58:16 +0700

    Ve hinh tam giac ngoi sao

---
+++ rectangDraw.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class rectangDraw {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the height: ");
+        int height = scanner.nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
